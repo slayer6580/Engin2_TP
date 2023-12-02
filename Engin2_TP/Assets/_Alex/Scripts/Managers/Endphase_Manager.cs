639e56d73ccea0c206bb97086d6e414e80fdd9a1
mqcfrank <88257038+mqcfrank@users.noreply.github.com>
2023-12-02 03:02:17 -0500

    Added Endphase mananger; singleton for now

---
+++ Engin2_TP/Assets/_Alex/Scripts/Managers/Endphase_Manager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Endphase_Manager : MonoBehaviour
+{
+    public static Endphase_Manager _Instance
+    {
+        get;
+        private set;
+    }
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (_Instance == null || _Instance == this)
+        {
+            _Instance = this;
+            return;
+        }
+        Destroy(this);
+    }
+}
